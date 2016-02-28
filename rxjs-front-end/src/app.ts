7396929e5ecf4995f5b15481b81089dbe09d4a06
jkbits1 <jkbits1@users.noreply.github.com>
2016-02-28 02:39:31 +0000

    building support for multiple inputs

--- rxjs-front-end/src/app.ts
+++ rxjs-front-end/src/app.ts
@@ -3,30 +3,40 @@
  */
 
 import {Component, FORM_DIRECTIVES, CORE_DIRECTIVES, Observable, EventEmitter} from 'angular2/angular2';
-import {Http, URLSearchParams} from 'angular2/http';
-import {JSONP_PROVIDERS, Jsonp} from 'angular2/http';
+//import {Http, URLSearchParams} from 'angular2/http';
+//import {JSONP_PROVIDERS, Jsonp} from 'angular2/http';
 
 @Component({
     selector: 'my-app',
-    providers: [JSONP_PROVIDERS],
+    //providers: [JSONP_PROVIDERS],
     template: `
 <div>
-<input #item1 type="text" (keyup)="keyup($event)">
+<input #wheel1 type="text" (keyup)="keyup1($event)">
+<input #wheel2 type="text" (keyup)="keyup2($event)">
+<input #wheel3 type="text" (keyup)="keyup3($event)">
+<input #wheel4 type="text" (keyup)="keyup4($event)">
 <ul>
   <li *ng-for="#result of results">{{result.val}}&nbsp;{{result.id}}</li>
 </ul>
-<pre>{{ item1.value }}</pre>
+<pre>{{ wheel1.value }}</pre>
 </div>
 `,
 directives: [CORE_DIRECTIVES, FORM_DIRECTIVES]
 })
 export class App {
-  item1input: EventEmitter = new EventEmitter();
+  wheel1input = new EventEmitter();
+  wheel2input = new EventEmitter();
+  wheel3input = new EventEmitter();
+  wheel4input = new EventEmitter();
 
-  constructor(http:Http, jsonp:Jsonp) {
+  id: number = 0;
+  results = [];
+
+  //constructor(http:Http, jsonp:Jsonp) {
+  constructor() {
     this.id = 0;
     this.results = [];
-    this.item1input._subject
+    this.wheel1input._subject
       .debounceTime(50)
       .distinctUntilChanged()
       //.switchMap(term => {
@@ -47,7 +57,19 @@ export class App {
       );
   }
 
-  keyup($event){
-    this.item1input.next($event.currentTarget.value);
+  keyup1 ($event) {
+    this.wheel1input.next($event.currentTarget.value);
+  }
+
+  keyup2 ($event) {
+    this.wheel2input.next($event.currentTarget.value);
+  }
+
+  keyup3 ($event) {
+    this.wheel3input.next($event.currentTarget.value);
+  }
+
+  keyup4 ($event) {
+    this.wheel4input.next($event.currentTarget.value);
   }
 }
